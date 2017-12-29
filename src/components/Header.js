1cfcd2de86bf35419a9bbdc709f2ca207ce13b64
Mike Tsou <mr.tsou@gmail.com>
2017-12-29 13:10:17 -0800

    Create Header component

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <img src="/images/igniter-logo-white_preview.png" alt="header"/>
+    </div>
+  );
+}
+
+export default Header;
