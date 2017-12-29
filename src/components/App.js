12516d50d4bef683c563952a32cc176fbec3a6a3
Mike Tsou <mr.tsou@gmail.com>
2017-12-29 13:27:23 -0800

    Create Slider component and a image json file

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import Slider from './slider/Slider';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header />
+        <Slider />
       </div>
     );
   }
---
+++ src/components/slider/Slider.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Slider extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      images: []
+    }
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div className="slider">
+
+      </div>
+    );
+  }
+}
+
+export default Slider;
