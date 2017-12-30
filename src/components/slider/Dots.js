1f17d42c918aafac0ed3fc478871a0d28cbf62e0
Mike Tsou <mr.tsou@gmail.com>
2017-12-29 16:00:01 -0800

    Create array of Dot components and pass necessary props to Dot

--- src/components/slider/Dots.js
+++ src/components/slider/Dots.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import Dot from './Dot'
 
 const Dots = ({ index, quantity, dotClick }) => {
   let dots = [];
 
+  for(let i = 0; i < quantity; i++) {
+    let isActive = i === index ? true : false;
 
+    dots.push(
+      <Dot
+        key={i}
+        active={isActive}
+        dotClick={dotClick}
+      />
+    );
+  }
 
   return (
     <div className="dots-container">
-
+      {dots}
     </div>
   );
 }
